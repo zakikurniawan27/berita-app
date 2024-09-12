9067b842572144f4b1010ab60568cef374a83652
zakikurniawan27 <kurniawan.zak279@gmail.com>
2024-09-12 08:55:59 +0800

    add middleware for authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'penulis'=> \App\Http\Middleware\Penulis::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,18 @@ Route::get('/registrasi', [Auth::class, 'registrasi'])->name('registrasi');
 Route::post('/reg', [Auth::class, 'createRegistrasi'])->name('createRegistrasi');
 Route::post('/logout', [Auth::class, 'logOut'])->name('logOut');
 
-Route::get('/dashboard', [Berita::class, 'penulisHome'])->name('penulisHome');
-Route::get('/dashboard/berita', [Berita::class, 'penulisBerita'])->name('penulisBerita');
+Route::middleware(['auth', 'penulis'])->group(function(){
+    Route::get('/dashboard', [Berita::class, 'penulisHome'])->name('penulisHome');
+    Route::get('/dashboard/berita', [Berita::class, 'penulisBerita'])->name('penulisBerita');
 
-Route::get('/dashboard/berita/create', [Berita::class, 'createBerita'])->name('createBerita');
-Route::post('/dashboard/berita/create/post', [Berita::class, 'storeBerita'])->name('storeBerita');
+    Route::get('/dashboard/berita/create', [Berita::class, 'createBerita'])->name('createBerita');
+    Route::post('/dashboard/berita/create/post', [Berita::class, 'storeBerita'])->name('storeBerita');
 
-Route::get('/dashboard/berita/edit/{id}', [Berita::class, 'editBerita'])->name('editBerita');
-Route::put('/dashboard/berita/edit/up/{id}', [Berita::class, 'updateBerita'])->name('updateBerita');
+    Route::get('/dashboard/berita/edit/{id}', [Berita::class, 'editBerita'])->name('editBerita');
+    Route::put('/dashboard/berita/edit/up/{id}', [Berita::class, 'updateBerita'])->name('updateBerita');
 
-Route::delete('/dashboard/berita/delete/{id}', [Berita::class, 'destroyBerita'])->name('destroyBerita');
+    Route::delete('/dashboard/berita/delete/{id}', [Berita::class, 'destroyBerita'])->name('destroyBerita');
 
-Route::get('/dashboard/comment', [Comment::class, 'comment'])->name('comment');
-Route::get('/dashboard/{id}/comment/detail', [Comment::class, 'showDetailComment'])->name('showDetailComment');
+    Route::get('/dashboard/comment', [Comment::class, 'comment'])->name('comment');
+    Route::get('/dashboard/{id}/comment/detail', [Comment::class, 'showDetailComment'])->name('showDetailComment');
+});
